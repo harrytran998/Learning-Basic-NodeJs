cabe2e55fc3208614d106a290a87540864254447
NhatTq <jetaimefrc@gmail.com>
2018-12-06 12:56:55 +0700

    [Update] Template engine PUG

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
-const path = require('path');
+const path = require('path')
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require('express')
+const bodyParser = require('body-parser')
 
-const app = express();
+const app = express()
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+app.set('view engine', 'pug')
+app.set('views', 'views')
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname, 'public')));
+const adminData = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
 
-app.use('/admin', adminRoutes);
-app.use(shopRoutes);
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-});
+app.use('/admin', adminData.routes)
+app.use(shopRoutes)
 
-app.listen(3000);
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+})
+
+app.listen(3000)
--- routes/shop.js
+++ routes/shop.js
@@ -1,13 +1,14 @@
-const path = require('path');
+// const path = require('path')
 
-const express = require('express');
+const express = require('express')
 
-const rootDir = require('../util/path');
+// const rootDir = require('../util/path')
 
-const router = express.Router();
+const router = express.Router()
 
-router.get('/', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
-});
+router.get('/', (req, res) => {
+  // res.sendFile(path.join(rootDir, 'views', 'shop.html'))
+  res.render('shop')  
+})
 
-module.exports = router;
+module.exports = router
