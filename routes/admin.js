adc5159a021cd422d2790f47646f2c305f165140
NhatTq <jetaimefrc@gmail.com>
2018-12-08 10:52:31 +0700

    [Update] Refactor code

--- routes/admin.js
+++ routes/admin.js
@@ -1,22 +1,15 @@
-const express = require('express')
+const path = require('path');
 
-const adminController = require('../controllers/admin')
+const express = require('express');
 
-const router = express.Router()
+const productsController = require('../controllers/products');
+
+const router = express.Router();
 
 // /admin/add-product => GET
-router.get('/add-product', adminController.getAddProduct)
-
-// /admin/products => GET
-router.get('/products', adminController.getProducts)
+router.get('/add-product', productsController.getAddProduct);
 
 // /admin/add-product => POST
-router.post('/add-product', adminController.postAddProduct)
+router.post('/add-product', productsController.postAddProduct);
 
-router.get('/edit-product/:productId', adminController.getEditProduct)
-
-router.post('/edit-product', adminController.postEditProduct)
-
-router.post('/delete-product', adminController.postDeleteProduct)
-
-module.exports = router
+module.exports = router;
--- routes/shop.js
+++ routes/shop.js
@@ -1,16 +1,11 @@
-// const path = require('path')
+const path = require('path');
 
-const express = require('express')
-const adminData = require('./admin')
+const express = require('express');
 
-// const rootDir = require('../util/path')
+const productsController = require('../controllers/products');
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', (req, res) => {
-  // res.sendFile(path.join(rootDir, 'views', 'shop.html'))
-  const products = adminData.products
-  res.render('shop', { prods: products, docTitle: 'Shop' })
-})
+router.get('/', productsController.getProducts);
 
-module.exports = router
+module.exports = router;
